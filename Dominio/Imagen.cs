2b5f85719a9dd7d1e07e512017df41b92d76f24a
jiriberri <julianiriberri@gmail.com>
2026-04-24 22:03:56 -0300

    Arreglando un bug en arreglar imagen

--- Dominio/Imagen.cs
+++ Dominio/Imagen.cs
@@ -8,9 +8,9 @@ namespace Dominio
 {
    public class Imagen
     {
-      public int Id { get; set; }
-      public int IdArticulo { get; set;}
-      public string ImagenUrl { get; set; }
-    
+        public int Id { get; set; }
+        public int IdArticulo { get; set;}
+        public string ImagenUrl { get; set; }
+
     }
 }
